eb8ae033d9e3bd70d30e5c9c2487cf81c6c9d7ae
jnakht <jisan.cse.cu@gmail.com>
2025-08-06 13:24:57 +0600

    cors installed

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, { Application, NextFunction, Request, Response } from "express"
 import { booksRoutes } from "./app/controllers/books.controllers";
 import { borrowsRoutes } from "./app/controllers/borrow.controllers";
 import { errorHandler } from "./error.hanlder.middleware";
+import cors from 'cors'
 
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors())
 app.use('/api/books', booksRoutes);
 app.use('/api/borrow', borrowsRoutes);
 
