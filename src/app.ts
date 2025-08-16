97adcb6fea148746ed42d68ad13860ecfbe7f1e8
jnakht <jisan.cse.cu@gmail.com>
2025-08-17 00:22:47 +0600

    frontend deploy link added

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,18 @@ import cors from 'cors';
 const app: Application = express();
 app.use(cors());
 
+// app.use(
+//   cors({
+//     origin: "*",
+//     // methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: "*",
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+    origin: ['http://localhost:5173', 'https://library-management-frontend-omega-ten.vercel.app/']
+   })
 );
 app.use(express.json());
 
